0e392592ca27898ead0aa41075d3f5aa047e405e
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-11-06 21:25:27 +0200

    WEB-14 | Storage MS now responds to POST req on /next-link endpoint with links from DB

--- source/storage/core/app/main.py
+++ source/storage/core/app/main.py
@@ -6,7 +6,7 @@ from decouple import AutoConfig
 import endpoint_constants
 
 
-config = AutoConfig(search_path='../../init/')
+config = AutoConfig(search_path='../../init/.env')
 
 MY_USER = config('MY_USER')
 MY_PASSWORD = config('MY_PASSWORD')
@@ -18,6 +18,8 @@ db = SQLAlchemy(app)
 
 
 class NextLinks(db.Model):
+    __tablename__ = 'next_links'
+
     id = db.Column(db.Integer, primary_key=True)
     url_site = db.Column(db.String(2083), nullable=False)
 
@@ -25,6 +27,8 @@ class NextLinks(db.Model):
         return '<Category %r>' % self.url_site
 
 class VisitedLinks(db.Model):
+    __tablename__ = 'visited_links'
+
     id = db.Column(db.Integer, primary_key=True)
     url_site = db.Column(db.String(2083), nullable=False)
 
@@ -46,5 +50,18 @@ def in_get_data() -> str:
 
     return jsonify({"ala": "bala"})
 
+@app.route(endpoint_constants.NEXT_LINK, methods=['POST'])
+def in_get_next_links() -> str:
+    json_resp = request.get_json('json_resp')
+
+    next_link_db_resp = NextLinks.query.limit(int(json_resp["quantity"])).all()
+
+    dict_next_url = dict()
+    dict_next_url["urls"] = []
+    for el in next_link_db_resp:
+        dict_next_url["urls"].append(el.url_site)
+
+    return dict_next_url
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='192.168.100.215', port=80, debug=True)
--- source/storage/core/tests/app_tests/test_main.py
+++ source/storage/core/tests/app_tests/test_main.py
@@ -1,4 +1,7 @@
+import json
+
 import endpoint_constants
+import constants
 
 def test_in_get_data(client):
     response = client.post(endpoint_constants.STORAGE)
@@ -9,3 +12,17 @@ def test_in_get_data(client):
     assert result is not None
     assert "ala" in result
     assert result['ala'] == "bala"
+
+
+def test_in_get_next_links(client):
+    send_data = {"quantity": constants.NEXT_LINK_LIMIT}
+    response = client.post(endpoint_constants.NEXT_LINK, data = json.dumps(send_data))
+    result = response.get_json()
+    assert result is not None
+
+    assert response.status_code == 200
+    assert result is not None
+    assert "urls" in result
+    assert result['urls'] == ["https://apple.com/", "https://www.google.com/", "https://www.youtube.com/"]
+
+
