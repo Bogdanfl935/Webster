75708c5e98bd0dcb212266aa2a7b18090e9c1f75
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-11-17 16:56:59 +0200

    WEB-21 | Introduced auth-db docker configuration

---
+++ source/auth-db/init/01-init.sql
@@ -0,0 +1,27 @@
+--=========================================[MISC DDL]==========================================
+CREATE EXTENSION citext;
+CREATE DOMAIN email AS citext
+	CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
+
+--=========================================[TABLE DDL]=========================================
+CREATE TABLE App_User(
+--	Field name						Field type			Field constraints
+	id 								BIGINT 		PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	username 						EMAIL 		UNIQUE NOT NULL,
+	password 						TEXT 		NOT NULL,
+	is_enabled						BOOLEAN		NOT NULL,
+	is_credentials_non_expired		BOOLEAN		NOT NULL
+);
+
+CREATE TABLE Role(
+--	Field name		Field type			Field constraints
+	id 				BIGINT 		PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	title 			CITEXT 		UNIQUE NOT NULL
+);
+
+CREATE TABLE App_User_Role(
+--	Field name		Field type			Field constraints
+	app_user_id 	BIGINT 		REFERENCES App_User,
+	role_id 		BIGINT 		REFERENCES Role,
+	PRIMARY KEY(app_user_id, role_id)
+);
