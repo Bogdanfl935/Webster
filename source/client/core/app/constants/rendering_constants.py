90f38078fd79fba86de2720578ac4d4da4c2712f
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-11-27 14:51:22 +0200

    WEB-30 | Refactored error landing page

--- source/client/core/app/constants/rendering_constants.py
+++
@@ -1,3 +0,0 @@
-# Error template constants
-ERROR_STATUS = 'error_status'
-ERROR_MESSAGE = 'error_message'
--- source/client/core/app/main.py
+++ source/client/core/app/main.py
@@ -1,36 +1,51 @@
-from flask import Flask, jsonify, request, render_template
-import endpoint_constants
-import constants
-import app_constants
+from flask import Flask, jsonify, render_template
+from app.constants import app_constants, endpoint_constants, rendering_constants, template_constants, static_constants
+from werkzeug.exceptions import HTTPException
 import random
 
 app = Flask(__name__)
 
+
 @app.context_processor
-def inject_endpoint_constants():
+def inject_constants():
     return dict(
-      home_endpoint = in_post_link.__name__,
-      crawled_content_endpoint = handle_crawled_content.__name__
+        static_constants=static_constants,
+        template_constants=template_constants,
+        home_endpoint=in_post_link.__name__,
+        data_crawled_endpoint=handle_data_crawled.__name__
     )
 
-@app.route("/error", methods=['GET'])
-def handle_error():
-  error_dict = {'error_status': 404, 'error_message': 'Page not found'}
-  return render_template('./error/error.html', error=error_dict)
 
-@app.route(endpoint_constants.CLIENT, methods=['GET'])
+@app.errorhandler(Exception)
+def handle_error(exception):
+    error_code = exception.code if isinstance(exception, HTTPException) else 500
+    error_dict = dict(
+        error_status=error_code,
+        error_message=str(exception)
+    )
+    return render_template(template_constants.ERROR_ERROR_PATH, error=error_dict)
+
+
+@app.route(endpoint_constants.DEFAULT, methods=['GET'])
+@app.route(endpoint_constants.HOME, methods=['GET'])
 def in_post_link() -> str:
-  return render_template("./section/home.html", logged_in=True)
+    return render_template(template_constants.SECTION_HOME_PATH, logged_in=True)
+
 
 @app.route(endpoint_constants.CRAWLED_CONTENT, methods=['GET'])
-def handle_crawled_content() -> str:
-  random_dict = dict()
-  for i in range(0, 15):
-    n = random.randint(1, 30)
-    m = random.randint(1, 100)
-    random_dict[n]=m
+def handle_data_crawled() -> str:
+    content_list = list()
+    hosts = ("Github", "Google", "Wikipedia", "Youtube", "Jira", "Facebook", "Instagram")
+    for i in range(0, 15):
+        content_list.append({
+            "page_host": random.choice(hosts),
+            "time_taken": random.randint(1, 30),
+            "quantity_found": random.randint(1, 100),
+        })
 
-  return render_template("./section/crawled_content.html", logged_in=False, table_vals=random_dict)
+    return render_template(template_constants.SECTION_CRAWLED_CONTENT_PATH, logged_in=False,
+                           crawled_content_list=content_list)
+
 
 if __name__ == '__main__':
-  app.run(host=app_constants.APP_HOST, port=app_constants.APP_PORT, debug=True)
+    app.run(host=app_constants.APP_HOST, port=app_constants.APP_PORT, debug=True)
