d57f4a896223dd03da43d3501d2814b241c7cb60
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-11-27 17:57:36 +0200

    WEB-31 | Refactoring of constants

--- source/client/core/app/constants/endpoint_constants.py
+++ source/client/core/app/constants/endpoint_constants.py
@@ -2,12 +2,23 @@ CRAWLER = "/crawler"
 PARSER = "/parser"
 NEXT_LINK = "/next-link"
 
-# Own endpoints
+# Own Endpoints
+# GET Endpoints
 DEFAULT = "/"
 HOME = "/home"
 ERROR = "/error"
 CRAWLED_CONTENT = "/crawled-content"
 
+# POST Endpoints
+REGISTRATION = "/registration"
+AUTHENTICATION = "/authentication"
 
+AUTHORIZATION = "/authorization"
+REFRESHMENT = "/refreshment"
+
+CONFIRMATION = "/confirmation"
+PASSWORD_RESET = "/password-reset"
+
+AUTH_MS_URL="http://localhost:5550"
 STORAGE_MS_URL= "http://localhost:50006"
 PARSER_MS_URL= "http://localhost:50005"
