2507a831b4d0009b29ddc8339022bf007c41c9d6
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-11-30 19:04:43 +0200

    WEB-34 | Refactoring

--- source/config/core/app/main.py
+++ source/config/core/app/main.py
@@ -1,41 +1,16 @@
-import json
-
-import requests
-from flask import Flask, jsonify, request
-import endpoint_constants
-import constants
-import app_constants
-from werkzeug.utils import environ_property
+from flask import Flask, request
+from app.constants import app_constants, endpoint_constants
+from app import config_service
 
 app = Flask(__name__)
 
 @app.route(endpoint_constants.CONFIGURATION, methods=['POST'])
 def handle_config_post() -> str:
-    text = request.get_json()
-
-    specific_tag = ["a"]
-    stay_on_same_page = False if "same-page" not in text else text["same-page"]
-    storage_limit = 100 if "storage-limit" not in text else text["storage-limit"]
-
-    if "specific-tag" in text.keys():
-        specific_tag.extend(text["specific-tag"])
-
-    config_json = json.dumps({"specific-tag": specific_tag, "same-page": stay_on_same_page, "storage-limit": storage_limit})
-
-    post_to_db = requests.post(url = f'{endpoint_constants.STORAGE_MS_URL}{endpoint_constants.STORE_CONFIGURATION}', data=config_json, headers={'Content-type': 'application/json'})
-
-    return config_json
+    return config_service.add_config_to_db(request)
 
 @app.route(endpoint_constants.RETRIEVE_CONFIGURATION_CONFIG, methods=['POST'])
-def handle_config_get() -> str:
-    recv_data_json = request.get_json()
-
-    retr_config_req = json.dumps({"user": "george"})
-
-    from_db_resp = requests.post(url = f'{endpoint_constants.STORAGE_MS_URL}{endpoint_constants.RETRIEVE_CONFIGURATION_DB}', data=retr_config_req, headers={'Content-type': 'application/json'})
-    from_db_resp_json = from_db_resp.json()
-
-    return from_db_resp_json
+def handle_config_retr_post() -> str:
+    return config_service.retr_config_from_db(request)
 
 if __name__ == '__main__':
     app.run(host=app_constants.APP_HOST, port=app_constants.APP_PORT, debug=True)
--- source/config/core/tests/app_tests/test_main.py
+++ source/config/core/tests/app_tests/test_main.py
@@ -1,13 +1,18 @@
-import constants
-import endpoint_constants
+from app.constants import endpoint_constants
+
 
 def test_handle_config_post(client):
     request_payload = {}
     response = client.post(endpoint_constants.CONFIGURATION, json=request_payload)
+    result = response.get_json(force=True)
+
+    assert response.status_code == 200
+    assert result is not None
+
+def test_handle_config_retr_post(client):
+    request_payload = {}
+    response = client.post(endpoint_constants.RETRIEVE_CONFIGURATION_CONFIG, json=request_payload)
     result = response.get_json()
 
     assert response.status_code == 200
     assert result is not None
-    assert "specific-tag" in result
-    assert "same-page" in result
-    assert "storage-limit" in result
