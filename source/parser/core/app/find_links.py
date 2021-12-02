e54bf154f7473966893f7566cb849e7fd27ed275
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-02 21:19:38 +0200

    WEB-35 | Parser will parse tags mentioned in config

--- source/parser/core/app/find_links.py
+++ source/parser/core/app/find_links.py
@@ -7,25 +7,58 @@ import endpoint_constants
 from lxml import html
 from bs4 import BeautifulSoup
 
+
 def parse_urls(text, url):
+    # TODO add gen on config MS
 
-  # TODO add gen on config MS
+    soup = BeautifulSoup(text, "lxml")
 
-  soup = BeautifulSoup(text, "lxml")
+    req_parser_config = requests.post(url=f'{endpoint_constants.CONFIG_MS_URL}{endpoint_constants.PARSER_CONFIG}',
+                                        data=json.dumps({}))
 
-  a_href_list = []
+    parsed_content = dict()
 
-  for link in soup.find_all("a"):
-    a_href_list.append(link.get("href").encode('latin1').decode('unicode-escape').replace('"', ''))
+    config_json = req_parser_config.json()
+    el_list = config_json["specific-tag"]
 
-  for link in a_href_list:
-    if link[0] == '.':
-      index = a_href_list.index(link)
-      link2=str(url) + link[1:]
-      a_href_list.remove(link)
-      a_href_list.insert(index, link2)
+    a_href_list = []
 
+    for link in soup.find_all("a"):
+        if link.get("href"):
+            a_href_list.append(link.get("href").encode('latin1').decode('unicode-escape').replace('"', ''))
 
-  next_links = {'links': a_href_list}
+    for link in a_href_list:
+        if link[0] == '.' or link[0] == '/':
+            index = a_href_list.index(link)
+            link2 = str(url) + link[1:]
+            a_href_list.remove(link)
+            a_href_list.insert(index, link2)
 
-  return next_links
+    parsed_content['a'] = a_href_list
+    el_list = list(filter(lambda a: a != 'a', el_list))
+
+    if "img" in el_list:
+        img_list = []
+        for img in soup.find_all("img"):
+            img_list.append(img.get("src").encode('latin1').decode('unicode-escape').replace('"', ''))
+
+        for img in img_list:
+            if img[0] == '.':
+                index = img_list.index(img)
+                img2 = str(url) + img[1:]
+                img_list.remove(img)
+                img_list.insert(img, img2)
+
+        parsed_content['img'] = img_list
+        el_list = list(filter(lambda a: a != 'img', el_list))
+
+    tags = dict()
+
+    for el in el_list:
+        tags[el] = []
+        for data in soup.find_all(el):
+            tags[el].append(data.text)
+
+        parsed_content[el] = tags[el]
+
+    return parsed_content
