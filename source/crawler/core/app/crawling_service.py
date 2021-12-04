26e21e52f102c23df97b2895057d5fbfb171cfd0
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-04 23:03:03 +0200

    WEB-48 | Code refactoring

--- source/crawler/core/app/crawling_service.py
+++ source/crawler/core/app/crawling_service.py
@@ -42,41 +42,33 @@ def start_crawling(url, html):
 
 
 async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
-    """GET request wrapper to fetch page HTML.
-
-    kwargs are passed to `session.request()`.
-    """
-
     resp = await session.request(method="GET", url=url, **kwargs)
     resp.raise_for_status()
     html = await resp.text()
     return html
 
 
-async def parse(url: str, session: ClientSession, **kwargs) -> set:
-    """Find HREFs in the HTML of `url`."""
-    found = 0
+async def process_crawling(url: str, session: ClientSession, **kwargs) -> set:
+    size = 0
     try:
         html = await fetch_html(url=url, session=session, **kwargs)
     except (
             aiohttp.ClientError,
             aiohttp.http_exceptions.HttpProcessingError,
     ) as e:
-        return found
+        return size
     except Exception as e:
-        return found
+        return size
     else:
-        found = start_crawling(url, html)
-        return found
+        size = start_crawling(url, html)
+        return size
 
 
-async def write_one(file: IO, url: str, **kwargs) -> None:
-    """Write the found HREFs from `url` to `file`."""
-    res = await parse(url=url, **kwargs)
+async def crawled_size(url: str, **kwargs) -> None:
+    res = await process_crawling(url=url, **kwargs)
     if not res:
-        return None
-    async with aiofiles.open(file, "a") as f:
-        await f.write(f"{url}\t{res}\n")
+        return 0
+
     return res
 
 
@@ -94,7 +86,7 @@ async def do_crawling(url):
         while len(urls) > 0 and max_total_size > 0:
             for url in urls:
                 tasks.append(
-                    functools.partial(write_one, file=file, url=url, session=session)
+                    functools.partial(crawled_size, url=url, session=session)
                 )
                 urls.remove(url)
             if len(urls) < 1:
