cb9ef0c628005e77be287e6f63eefd6814d6222d
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-04 22:56:54 +0200

    WEB-48 | Async crawler will stop when max size from config is reached

--- source/crawler/core/app/crawling_service.py
+++ source/crawler/core/app/crawling_service.py
@@ -12,8 +12,7 @@ import aiofiles
 import aiohttp
 from aiohttp import ClientSession
 from typing import IO
-
-HREF_RE = re.compile(r'href="(.*?)"')
+from app.get_total_size import total_size
 
 
 def get_config():
@@ -22,6 +21,7 @@ def get_config():
     json_config = req_crawling_config.json()
 
     return json_config
+
 
 def start_crawling(url, html):
     dictPage = dict()
@@ -36,9 +36,10 @@ def start_crawling(url, html):
     send_to_parser = requests.post(url=parser_url, data=dictPage, headers={'Content-type': 'application/json'})
     parser_resp_json = send_to_parser.json()
 
-    resp_size = sys.getsizeof(parser_resp_json)
+    resp_size = total_size(parser_resp_json)
 
     return resp_size
+
 
 async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
     """GET request wrapper to fetch page HTML.
@@ -51,20 +52,21 @@ async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
     html = await resp.text()
     return html
 
+
 async def parse(url: str, session: ClientSession, **kwargs) -> set:
     """Find HREFs in the HTML of `url`."""
-    found = set()
+    found = 0
     try:
         html = await fetch_html(url=url, session=session, **kwargs)
     except (
-        aiohttp.ClientError,
-        aiohttp.http_exceptions.HttpProcessingError,
+            aiohttp.ClientError,
+            aiohttp.http_exceptions.HttpProcessingError,
     ) as e:
         return found
     except Exception as e:
         return found
     else:
-        found.add(start_crawling(url, html))
+        found = start_crawling(url, html)
         return found
 
 
@@ -74,24 +76,22 @@ async def write_one(file: IO, url: str, **kwargs) -> None:
     if not res:
         return None
     async with aiofiles.open(file, "a") as f:
-        for p in res:
-            await f.write(f"{url}\t{p}\n")
+        await f.write(f"{url}\t{res}\n")
+    return res
 
-async def wrap(url):
-    await asyncio.run(do_crawling(url))
 
 async def do_crawling(url):
-    urls=[url]
+    urls = [url]
     file = "output.txt"
 
-    # json_config = get_config()
-    #
-    # max_total_size = int(json_config["storage-limit"][0])
-    # max_total_size = max_total_size * 10 ** 6
+    json_config = get_config()
+
+    max_total_size = int(json_config["storage-limit"][0])
+    max_total_size = max_total_size * 10 ** 6
 
     async with ClientSession() as session:
         tasks = []
-        while len(urls) > 0:
+        while len(urls) > 0 and max_total_size > 0:
             for url in urls:
                 tasks.append(
                     functools.partial(write_one, file=file, url=url, session=session)
@@ -100,37 +100,28 @@ async def do_crawling(url):
             if len(urls) < 1:
                 next_links_from_db = get_next_link()
                 urls = next_links_from_db["urls"]
-            await asyncio.gather(*[func() for func in tasks])
-
-    #
-    # crawled_size = start_crawling(url)
-    # while crawled_size < max_total_size:
-    #     if crawled_size == -1:
-    #         next_urls = get_next_link()
-    #         for el in next_urls["urls"]:
-    #             crawled_size = await start_crawling(el)
-    #     else:
-    #         max_total_size = max_total_size - crawled_size
-    #         next_urls = get_next_link()
-    #         for el in next_urls["urls"]:
-    #             crawled_size = await start_crawling(el)
+            result = await asyncio.gather(*[func() for func in tasks])
+            max_total_size = max_total_size - result[-1]
 
     return ('', 200)
+
 
 def get_next_link():
     json_config = get_config()
 
     if 'True' in json_config["same-page"]:
         return json.dumps({"urls": []})
-    
+
     post_to_next_link = {'quantity': constants.CRAWLER_NEXT_LINK_LIMIT}
 
-    req_next_links = requests.post(url=f'{endpoint_constants.STORAGE_MS_URL}{endpoint_constants.NEXT_LINK}', data=json.dumps(post_to_next_link))
+    req_next_links = requests.post(url=f'{endpoint_constants.STORAGE_MS_URL}{endpoint_constants.NEXT_LINK}',
+                                   data=json.dumps(post_to_next_link))
     if req_next_links.status_code == 200:
         next_links = req_next_links.json()
     elif req_next_links.status_code != 200:
         time.sleep(2)
-        req_next_links = requests.post(url=f'{endpoint_constants.STORAGE_MS_URL}{endpoint_constants.NEXT_LINK}', data=json.dumps(post_to_next_link))
+        req_next_links = requests.post(url=f'{endpoint_constants.STORAGE_MS_URL}{endpoint_constants.NEXT_LINK}',
+                                       data=json.dumps(post_to_next_link))
         if req_next_links.status_code != 200:
             next_links = {'error_code': req_next_links.status_code}
         else:
