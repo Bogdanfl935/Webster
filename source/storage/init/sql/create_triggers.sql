277b73c962bd0ac7c0d45fd5341862caa828e2c9
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-08 22:24:39 +0200

    WEB-47 | Modified triggered procedure to check for duplicates in both next_links and visited_links

--- source/storage/init/sql/create_triggers.sql
+++ source/storage/init/sql/create_triggers.sql
@@ -1,13 +1,17 @@
--- Creation of function that checks for
+-- Creation of function that checks for duplicates in next_links and visited_links
 CREATE FUNCTION verify_duplicate()
     RETURNS trigger AS
 $BODY$
 BEGIN
     IF EXISTS(SELECT 1 FROM next_links WHERE new.url_site = url_site) THEN
         RETURN NULL;
-    ELSE
-        RETURN NEW;
     END IF;
+
+    IF EXISTS(SELECT 1 FROM visited_links WHERE new.url_site = url_site) THEN
+        RETURN NULL;
+    END IF;
+
+    RETURN NEW;
 END;
 $BODY$
     LANGUAGE plpgsql;
