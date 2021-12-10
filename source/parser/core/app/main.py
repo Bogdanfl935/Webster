ebeaa05cef79087e09a6dcf12d91cbaeb4056998
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-10 22:57:37 +0200

    WEB-56 | Parser uses redis from crawler to store last parsed content. Reformatted data returned on get req

--- source/parser/core/app/main.py
+++ source/parser/core/app/main.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request
 from app.find_links import parsing_service, get_last_parsed
+from app.config import app
 import endpoint_constants
 import constants
 import app_constants
-
-app = Flask(__name__)
 
 @app.route(endpoint_constants.PARSER, methods=['POST'])
 def handle_parser_post() -> str:
@@ -19,7 +18,9 @@ def handle_parser_post() -> str:
 
 @app.route(endpoint_constants.PARSER, methods=['GET'])
 def handle_parser_get() -> str:
-    return get_last_parsed()
+    username = request.args.get('username')
+
+    return get_last_parsed(username)
 
 if __name__ == '__main__':
     app.run(host=app_constants.APP_HOST, port=app_constants.APP_PORT, debug=True)
