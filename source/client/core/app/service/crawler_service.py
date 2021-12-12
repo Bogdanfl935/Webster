f6d70faad7a0242afe081dfdc83f232e357b8101
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-12-12 15:46:11 +0200

    WEB-59 | Introduced client side validation for crawler start

--- source/client/core/app/service/crawler_service.py
+++ source/client/core/app/service/crawler_service.py
@@ -3,7 +3,7 @@ import random
 def make_status_get(authenticated_user: str) -> dict:
     # Return dummy data until microservices are integrated
     letters = "abcdefghijklmonpqrstuvwxyz"
-    data = dict(active=True, memoryUsage=random.randint(1, 500), domain=str(random.choice(letters)*random.randint(1, 10)),
+    data = dict(active=False, memoryUsage=random.randint(1, 500), domain=str(random.choice(letters)*random.randint(1, 10)),
                 url=str(random.choice(letters)*random.randint(1, 10)))
     return data
 
