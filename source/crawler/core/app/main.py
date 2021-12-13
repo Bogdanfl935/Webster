82b0c34d8e827b05a0e8a79aafdc42b66ccac2b0
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-13 18:15:18 +0200

    WEB-52 | Added basic error handling capabilities to crawler

--- source/crawler/core/app/main.py
+++ source/crawler/core/app/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from app.crawling_service import do_crawling, get_next_link, get_config, get_last_crawled, stop_crawling
 from app.config import app
+from werkzeug.exceptions import HTTPException
+from app.error_handler import ErrorHandler
 import endpoint_constants
 import constants
 import app_constants
@@ -27,6 +29,11 @@ def handle_crawler_status_get() -> str:
 @app.route(endpoint_constants.CRAWLER_STOP, methods=['POST'])
 def handle_crawler_stop_post() -> str:
     return stop_crawling()
+
+@app.errorhandler(401)
+def handle_unauthorized_error(exception: HTTPException) -> str:
+    myError = ErrorHandler()
+    return json.dumps(myError.__dict__)
 
 if __name__ == '__main__':
     app.run(host=app_constants.APP_HOST, port=app_constants.APP_PORT, debug=True)
