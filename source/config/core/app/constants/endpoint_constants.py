4f2257f12a823436579db9e4858b57133249ffc0
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-12-13 19:16:11 +0200

    WEB-64 | Modified config endpoint constants + refactoring

--- source/config/core/app/constants/endpoint_constants.py
+++ source/config/core/app/constants/endpoint_constants.py
@@ -3,7 +3,9 @@ PARSER = "/parser"
 CONFIGURATION = "/config"
 STORE_CONFIGURATION = "/save-config"
 RETRIEVE_CONFIGURATION_DB = "/retr-config-db"
-RETRIEVE_CONFIGURATION_CRAWLER = "/crawler-config"
-RETRIEVE_CONFIGURATION_PARSER = "/parser-config"
+
+CRAWLER_CONFIGURATION = "/crawler-configuration"
+PARSER_CONFIGURATION = "/parser-configuration"
+
 PARSER_MS_URL= "http://localhost:50005"
 STORAGE_MS_URL = "http://localhost:50006"
