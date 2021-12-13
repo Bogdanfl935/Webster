374ecdaa5e5346908eff51f455a9f15ce599227d
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-12-13 19:00:38 +0200

    WEB-64 | Modified crawler endpoint constants + refactoring

--- source/crawler/core/app/main.py
+++ source/crawler/core/app/main.py
@@ -7,8 +7,8 @@ import app_constants
 import asyncio
 
 
-@app.route(endpoint_constants.CRAWLER, methods=['POST'])
-def handle_crawler_post() -> str:
+@app.route(endpoint_constants.CRAWLER_START, methods=['POST'])
+def handle_crawler_start_post() -> str:
     text = request.json.get(constants.START_LINK_KEY, None)
     # crawled_response = do_crawling(text)
 
@@ -18,13 +18,13 @@ def handle_crawler_post() -> str:
     return ('', 200)
 
 
-@app.route(endpoint_constants.CRAWLER, methods=['GET'])
-def handle_crawler_get() -> str:
+@app.route(endpoint_constants.CRAWLER_STATUS, methods=['GET'])
+def handle_crawler_status_get() -> str:
     username = request.args.get("username")
     return get_last_crawled(username)
 
-@app.route(endpoint_constants.STOP_CRAWLING, methods=['GET'])
-def handle_stop_crawling_get() -> str:
+@app.route(endpoint_constants.CRAWLER_STOP, methods=['POST'])
+def handle_crawler_stop_post() -> str:
     return stop_crawling()
 
 if __name__ == '__main__':
--- source/crawler/core/endpoint_constants.py
+++ source/crawler/core/endpoint_constants.py
@@ -1,8 +1,12 @@
-CRAWLER = "/crawler"
 PARSER = "/parser"
 NEXT_LINK = "/next-link"
 CRAWLER_CONFIG = "/crawler-config"
-STOP_CRAWLING = "/stop-crawling"
+
+
+CRAWLER_START = "/crawler-start"
+CRAWLER_STOP = "/crawler-stop"
+CRAWLER_STATUS = "/crawler-status"
+
 STORAGE_MS_URL = "http://localhost:50006"
 PARSER_MS_URL = "http://localhost:50005"
 CONFIG_MS_URL = "http://localhost:50004"
