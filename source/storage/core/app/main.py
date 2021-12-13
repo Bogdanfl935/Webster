a92612a95c78c978308b8714a5f8509a3cdefb6f
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-13 16:43:41 +0200

    WEB-52 | Added link validation and error handling on stoarage MS

--- source/storage/core/app/main.py
+++ source/storage/core/app/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from app.constants import app_constants, endpoint_constants
 from app import config_service, storage_access_service
 from app.config import app
+from werkzeug.exceptions import HTTPException
+from app.error_handler import ErrorHandler
+import time
+from datetime import datetime
 
 # users = User.query.all()
 
@@ -20,6 +24,25 @@ def handle_store_config_post() -> str:
 @app.route(endpoint_constants.RETR_CONFIGURATION, methods=['POST'])
 def handle_retr_config_post() -> str:
     return config_service.get_config_from_db(request)
+
+@app.errorhandler(400)
+def handle_unauthorized_error(exception: HTTPException) -> str:
+    myError = ErrorHandler(timestamp=datetime.fromtimestamp(time.time()), status=exception.code, error="Bad Request",
+                           errors=[exception.description])
+    return jsonify(myError.__dict__)
+
+
+# @app.errorhandler(Exception)
+# def handle_generic_error(exception) -> str:
+#     error_code = exception.code if isinstance(
+#         exception, HTTPException) else 500
+#
+#     path = exception.request.path_url if isinstance(
+#         exception, HTTPException) else None
+#
+#     myError = ErrorHandler(timestamp=datetime.fromtimestamp(time.time()), status=error_code, error="Internal Server Error",
+#                            message=str(exception), path=path)
+#     return jsonify(myError.__dict__)
 
 
 if __name__ == '__main__':
