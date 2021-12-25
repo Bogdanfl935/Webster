4d14c99fc1bdca31ca6acefe9042b553cd32faa2
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2021-12-25 14:48:26 +0200

    WEB-73 | Refactoring - Directory layout change

--- source/parser/core/app/error_handler.py
+++
@@ -1,8 +0,0 @@
-class ErrorHandler:
-    def __init__(self, timestamp, status, error, message=None, path=None, errors=[]):
-        self.timestamp = timestamp
-        self.status = status
-        self.error = error
-        self.message = message
-        self.path = path
-        self.errors = errors
