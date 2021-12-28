4fa7c7290faab662c94a3f5f15f2b9d1e08cbe23
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-28 16:19:11 +0200

    WEB-72 | Initial MS setup

---
+++ source/statistics/core/statistics/statistics/Program.cs
@@ -0,0 +1,62 @@
+using statistics.services;
+
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+HttpClient client = new HttpClient();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+
+app.MapGet("/public-stats", async () =>
+{
+    return await client.GetStringAsync("http://example.com");
+    //var pubStats = new KnownUserService();
+    //return pubStats.GetStatistics("example.com", client);
+})
+    .WithName("GetPublicStatistics");
+
+app.MapGet("/private-stats", () =>
+{
+
+})
+    .WithName("GetPrivateStatistics");
+
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ source/statistics/core/statistics/statistics/services/KnownUserService.cs
@@ -0,0 +1,18 @@
+﻿namespace statistics.services
+{
+    public class KnownUserService
+    {
+        // values = new Dictionary<string, string>
+        //{
+        //    { "thing1", "hello" },
+        //    { "thing2", "world" }
+        //};
+
+
+        internal async static Task GetStatistics(string url,
+                                          HttpClient client)
+        {
+            
+        }
+    }
+}
