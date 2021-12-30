c94cafdfe87af6ae1a0159e8379e9bcc6079868d
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2021-12-30 20:37:40 +0200

    WEB-27 | Docker compose for exporter

---
+++ source/exporter/core/exporter/exporter/dto/ExporterImagesDto.cs
@@ -0,0 +1,13 @@
+﻿namespace exporter.dto
+{
+    public class ExporterImagesDto
+    {
+        public long numberNextUrls { get; set; }
+        public long numberVisitedUrls { get; set; }
+        public ExporterImagesDto(long numberNextUrls, long numberVisitedUrls)
+        {
+            this.numberNextUrls = numberNextUrls;
+            this.numberVisitedUrls = numberVisitedUrls;
+        }
+    }
+}
