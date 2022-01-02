d8218d74e2cc45298374cb40027a5a6d2aee7505
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2022-01-02 22:37:06 +0200

    WEB-27 | Modfied multiple MS to have a working login/register flow

--- source/statistics/core/statistics/statistics/constants/AppConstants.cs
+++ source/statistics/core/statistics/statistics/constants/AppConstants.cs
@@ -2,7 +2,7 @@
 {
     class AppConstants
     {
-        public const string appURL = "http://192.168.100.215";
+        public const string appURL = "http://localhost";
         public const string appPort = "50007";
     }
 }
