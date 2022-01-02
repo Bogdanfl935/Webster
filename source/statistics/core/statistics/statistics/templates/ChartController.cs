fa133cddea47838f296bc3ba289a95771f7db2d4
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2022-01-02 17:29:04 +0200

    WEB-80 | Basic setup for chartjs generating

---
+++ source/statistics/core/statistics/statistics/templates/ChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace statistics.templates
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
