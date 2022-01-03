331b502eac6ad170aefc733568e83b8af7a3f22c
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2022-01-03 23:33:54 +0200

    WEB-27 | Functional crawling/parsing workflow established

--- source/parser/core/app/constants/endpoint_constants.py
+++ source/parser/core/app/constants/endpoint_constants.py
@@ -12,7 +12,7 @@ LAST_PARSED = "/last-parsed"
 
 PARSER_CONFIGURATION = "/parser-configuration"
 
-STORAGE_MS_URL= "http://127.0.0.1:50006"
-PARSER_MS_URL= "http://127.0.0.1:50005"
-CACHE_MS_URL = "http://127.0.0.1:50003/"
-CONFIG_MS_URL = "http://127.0.0.1:50004"
+STORAGE_MS_URL= "http://storage-ms:50006"
+PARSER_MS_URL= "http://parser-ms:50005"
+CACHE_MS_URL = "http://cache-ms:50003/"
+CONFIG_MS_URL = "http://config-ms:50004"
