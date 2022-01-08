b4794e3946a98759be22de4a9dc87b0733f0d225
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2022-01-08 17:08:11 +0200

    WEB-83 | Updated clientside activity rendering

--- source/client/core/app/static/scripts/activity_active.js
+++ source/client/core/app/static/scripts/activity_active.js
@@ -16,11 +16,26 @@ function initActivityHandlers() {
     startElapsedTimeCounter();
     enableCrawlerStatusPolling();
     bindParserFormSubmitHandle();
+    bindCrawlerStopFormSubmitHandle();
+}
+
+function bindCrawlerStopFormSubmitHandle(){
+    let form = $('#crawlerStopForm');
+    form.on('submit', (event) => {
+        formSubmitHandler(
+            event, 
+            form,
+            () => {}, /* Do nothing */
+            (_form, data)=>{console.log(data); location.reload();}, /* Error handle */
+            () => {$(form).find("input[required]").addClass('was-validated');} /* Callback */
+        );
+    });
 }
 
 function enableCrawlerStatusPolling(){
     let pollingFrequency = 1000 * MILLISECONDS;
     let continuePolling = true;
+    makeAjaxRequest($("#crawlerStatusForm"), handleCrawlerStatusFormSubmitSuccessResponse, (form, data)=>{console.log(data)});
     let intervalHandle = setInterval(()=>{
         if(continuePolling){
             makeAjaxRequest($("#crawlerStatusForm"), handleCrawlerStatusFormSubmitSuccessResponse, (form, data)=>{console.log(data)});
@@ -47,20 +62,33 @@ function bindParserFormSubmitHandle(){
 function handleParserStatusFormSubmitSuccessResponse(_form, data){
     let tableBody = $('#parserStatusTable');
     tableBody.empty();
-    data.content.forEach((entry, index)=>{
-        let col_1 = `<th class="col-md-1" scope="row">${index}</th>`
-        let col_2 = `<td class="col-md-1"><span class="badge badge-dark">&lt;${entry.tag}&gt;</span></td>`
-        let col_3 = `<td class="col-md-1"><span class="badge badge-danger">${(entry.size/1024).toFixed(2)+"kB"}</span></td>`
-        let col_4 = `<td class="col-md-1"><span class="badge badge-info" role='button' data-toggle="tooltip"
-        data-placement="bottom" title="${data.url}">${data.domain}</span></td>`
-        tableBody.append(`<tr>${col_1}${col_2}${col_3}${col_4}</tr>`);
-    });
+    renderParserTableContent(data, tableBody);
     enableTooltips();
 }
 
+function renderParserTableContent(data, tableBody){
+    let rowsCount = $('#parserStatusTable tr').length;
+    Object.keys(data).forEach((dict_key, _)=>{
+        data[dict_key].forEach((entry, _)=>{
+            let col_1 = `<th class="col-md-1" scope="row">${rowsCount++}</th>`
+            let col_2 = `<td class="col-md-1"><span class="badge badge-dark">&lt;${entry.tag}&gt;</span></td>`
+            let col_3 = `<td class="col-md-1"><span class="badge badge-danger">${(entry.memoryUsage/1024).toFixed(2)+"kB"}</span></td>`
+            let col_4 = `<td class="col-md-1"><span class="badge badge-info" role='button' data-toggle="tooltip"
+            data-placement="bottom" title="${dict_key}">${_getHostName(dict_key)}</span></td>`
+            tableBody.prepend(`<tr>${col_1}${col_2}${col_3}${col_4}</tr>`);
+        });
+    });
+}
+
 function handleCrawlerStatusFormSubmitSuccessResponse(form, data){
-    $("#crawlerMemoryUsage span").text((parseFloat(data.memoryUsage)/1024).toFixed(2).toString()+"kB");
-    $("#crawlerCurrentCrawl").text(data.domain).attr("data-bs-original-title", data.url);
+    if(data.active == true){
+        $("#crawlerMemoryUsage span").text((parseFloat(data.memoryUsage)/1024).toFixed(2).toString()+"kB");
+        $("#crawlerCurrentCrawl").text(_getHostName(data.lastUrl)).attr("data-bs-original-title", data.lastUrl);
+    }
+    else{
+        location.reload();
+    }
+    
 }
 
 function enableTooltips(){
@@ -88,4 +116,10 @@ function formatElapsedTime(elapsedtime, timeunit, base) {
   time = Math.floor(time);
   time = time < 10 ? '0' + time : time;
   return time;
+}
+
+function _getHostName(url){
+    let anchor = document.createElement('a');
+    anchor.href = url;
+    return anchor.hostname;
 }
