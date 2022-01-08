a84febe74cdddfa32779314c9e60b3306a35fef6
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2022-01-08 17:07:40 +0200

    WEB-83 | Updated crawler and parser services to correspond to latest changes + Additional refactoring

--- source/client/core/app/service/crawler_service.py
+++ source/client/core/app/service/crawler_service.py
@@ -1,54 +1,21 @@
-from flask import request, abort
-from app.constants import endpoint_constants
+from flask import request
+from app.constants import endpoint_constants, serialization_constants
 from app.utilities.url_joiner import construct_parameterized_url
+from app.utilities.api_utilities import unpack_response
 import requests
 
 def make_status_get(authenticated_user: str) -> dict:
-    target_url = construct_parameterized_url(endpoint_constants.CRAWLER_MS_URL + endpoint_constants.CRAWLER_STATUS,\
-        dict(username=authenticated_user))
+    target_url = construct_parameterized_url(endpoint_constants.CRAWLER_MS_URL + endpoint_constants.CRAWLER_STATUS,
+        {serialization_constants.USERNAME_KEY: authenticated_user})
     response = requests.get(target_url)
-    return_content = None
-    
-    match response.status_code:
-        case 200:
-            return_content = response.json()
-        case 400:
-            print(response.json())
-            abort(400)
-        case _:
-            abort(response.status_code)
-    
-    return return_content, response.status_code
+    return unpack_response(response)
 
 def make_crawler_start_post(authenticated_user: str) -> dict:
-    request_body = dict(username=authenticated_user) | request.form
+    request_body = {serialization_constants.USERNAME_KEY: authenticated_user} | request.form
     response = requests.post(endpoint_constants.CRAWLER_MS_URL + endpoint_constants.CRAWLER_START, json=request_body)
-    return_content = None
-    
-    match response.status_code:
-        case 200:
-            return_content = response.json()
-        case 400:
-            print(response.json())
-            abort(400)
-        case _:
-            pass
-            #abort(response.status_code)
-    
-    return return_content, response.status_code
+    return unpack_response(response)
 
 def make_crawler_stop_post(authenticated_user: str) -> dict:
-    request_body = dict(username=authenticated_user)
+    request_body = {serialization_constants.USERNAME_KEY: authenticated_user}
     response = requests.post(endpoint_constants.CRAWLER_MS_URL + endpoint_constants.CRAWLER_STOP, json=request_body)
-    return_content = None
-    
-    match response.status_code:
-        case 200:
-            return_content = response.json()
-        case 400:
-            print(response.json())
-            abort(400)
-        case _:
-            abort(response.status_code)
-    
-    return return_content, response.status_code
+    return unpack_response(response)
--- source/client/core/app/service/parser_service.py
+++ source/client/core/app/service/parser_service.py
@@ -1,21 +1,10 @@
-from flask import request, abort
 from app.constants import endpoint_constants
 from app.utilities.url_joiner import construct_parameterized_url
+from app.utilities.api_utilities import unpack_response
 import requests
 
 def make_status_get(authenticated_user: str) -> dict:
     target_url = construct_parameterized_url(endpoint_constants.PARSER_MS_URL + endpoint_constants.PARSER_STATUS,\
         dict(username=authenticated_user))
     response = requests.get(target_url)
-    return_content = None
-    
-    match response.status_code:
-        case 200:
-            return_content = response.json()
-        case 400:
-            print(response.json())
-            abort(400)
-        case _:
-            abort(response.status_code)
-    
-    return return_content, response.status_code
+    return unpack_response(response)
