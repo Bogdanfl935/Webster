adb2196196845307bdcf501f589821f73c9fccd6
Bogdan-Flavius <b.bogdanfl935@gmail.com>
2022-01-09 23:08:17 +0200

    WEB-88 | Introduced additional constants for archive integration

---
+++ source/client/core/app/constants/archive_endpoint_handler_constants.py
@@ -0,0 +1,4 @@
+# Scanned bound route handlers for archive:
+HANDLE_EXPORT_CONTENT_GET = 'archive.handle_export_content_get'
+HANDLE_PARSED_CONTENT_DELETE = 'archive.handle_parsed_content_delete'
+HANDLE_PARSED_IMAGE_DELETE = 'archive.handle_parsed_image_delete'
