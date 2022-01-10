71e53235dc981d43d6d71a8c5c8e6824eb1035ab
Mihai Damian <76659068+mihaidam@users.noreply.github.com>
2022-01-10 11:54:47 +0200

    WEB-94 | Updated output format to JSON

--- source/exporter/core/exporter/exporter/dto/ExporterContentDto.cs
+++ source/exporter/core/exporter/exporter/dto/ExporterContentDto.cs
@@ -2,11 +2,14 @@
 {
     public class ExporterContentDto
     {
-        public byte[] encodedFile { get; set; }
+        public Dictionary<string, string> encodedFile { get; set; }
         public ExporterContentDto(Microsoft.AspNetCore.Mvc.FileContentResult fileRez)
         {
             //this.encodedFile = System.Text.Encoding.UTF8.GetString(fileArray, 0, fileArray.Length);
-            this.encodedFile = fileRez.FileContents;
+            byte[] fileContent = fileRez.FileContents;
+            string base64Encoded = Convert.ToBase64String(fileContent);
+            this.encodedFile = new Dictionary<string, string>();
+            this.encodedFile.Add("parsedContent", base64Encoded);
         }
     }
 }
